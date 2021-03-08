5624ad18db4a9983f65e69228d943c71178cda59
baelanche <baelanche@naver.com>
2021-03-08 16:38:46 +0900

    20210308

--- boj/p2309/p2309.cpp
+++ boj/p2309/p2309.cpp
@@ -5,25 +5,23 @@ using namespace std;
 vector<int> a(9);
 vector<int> ans;
 void search(vector<int> c, int idx, int sum) {
-	c.push_back(a[idx]);
-	sum += a[idx];
 	if (sum > 100) return;
 	if (sum == 100) {
 		ans = c;
 		for (int i = 0; i < c.size(); i++)
-			cout << c[i] << ' ';
+			cout << c[i] << '\n';
 		return;
 	}
+	c.push_back(a[idx]);
+	sum += a[idx];
 	search(c, idx + 1, sum);
 	c.pop_back();
 	sum -= a[idx];
+	search(c, idx, sum);
 }
 int main() {
 	for (int i = 0; i < 9; i++) {
 		cin >> a[i];
 	}
 	search(ans, 0, 0);
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << ' ';
-	}
 }
