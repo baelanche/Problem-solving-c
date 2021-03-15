9662a1d48b2159d63053c5df5bae6a77942f21e7
baelanche <baelanche@naver.com>
2021-03-15 15:42:40 +0900

    20210315

--- boj/p3085/p3085.cpp
+++ boj/p3085/p3085.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int n;
 char a[50][50];
-int arrayCheck(int max) {
+void arrayCheck(int* max) {
 	int cnt;
 	for (int i = 0; i < n; i++) {
 		cnt = 1;
 		for (int j = 1; j < n; j++) {
 			if (a[i][j - 1] == a[i][j]) {
 				cnt++;
-				max = cnt > max ? cnt : max;
+				*max = cnt > *max ? cnt : *max;
 			}
 			else cnt = 1;
 		}
@@ -19,27 +19,26 @@ int arrayCheck(int max) {
 		for (int j = 1; j < n; j++) {
 			if (a[j - 1][i] == a[j][i]) {
 				cnt++;
-				max = cnt > max ? cnt : max;
+				*max = cnt > *max ? cnt : *max;
 			}
 			else cnt = 1;
 		}
 	}
-	return max;
 }
 int main() {
+	int max = 0;
 	cin >> n;
-	int max = 1;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			cin >> a[i][j];
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 1; j < n; j++) {
-			char temp1 = a[i][j-1];
+			char temp1 = a[i][j - 1];
 			a[i][j - 1] = a[i][j];
 			a[i][j] = temp1;
 
-			arrayCheck(max);
+			arrayCheck(&max);
 
 			char temp2 = a[i][j - 1];
 			a[i][j - 1] = a[i][j];
@@ -50,10 +49,10 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 1; j < n; j++) {
 			char temp1 = a[j - 1][i];
-			a[j-1][i] = a[j][i];
+			a[j - 1][i] = a[j][i];
 			a[j][i] = temp1;
 
-			arrayCheck(max);
+			arrayCheck(&max);
 
 			char temp2 = a[j - 1][i];
 			a[j - 1][i] = a[j][i];
