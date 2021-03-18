69643cc2d78b200045f1b7c8e4937f720465fa54
baelanche <baelanche@naver.com>
2021-03-18 22:06:09 +0900

    20210318

--- boj/p2503/p2503.cpp
+++ boj/p2503/p2503.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+typedef struct {
+	int number;
+	int strike;
+	int ball;
+} Game;
+vector<Game> v;
+int possible;
+int cnt;
+int n;
+void check(string);
 int main() {
-	int n;
 	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		Game g;
+		g.number = a;
+		g.strike = b;
+		g.ball = c;
+		v.push_back(g);
+	}
+
+	for (int j = 123; j <= 987; j++)
+		check(to_string(j));
+
+	cout << cnt;
+}
+void check(string candidate) {
+	for (int i = 0; i < v.size(); i++) {
+		if (candidate[0] == candidate[1] || candidate[0] == candidate[2] || candidate[1] == candidate[2]
+			|| candidate[1] == '0' || candidate[2] == '0')
+			continue;
+		string number = to_string(v[i].number);
+		int strike = v[i].strike;
+		int ball = v[i].ball;
+
+		int sameidx = 0;
+		int sameval = 0;
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				if (candidate[j] == number[k]) {
+					if (j == k) sameidx++;
+					else sameval++;
+				}
+			}
+		}
+		if (sameidx == strike && sameval == ball) possible++;
+	}
+	if (possible == n)
+		cnt++;
+	possible = 0;
 }
