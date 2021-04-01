30ba52e9a28b9c83f1052ac048aeb6a344ed663e
baelanche <baelanche@naver.com>
2021-04-01 23:03:31 +0900

    20210401

--- boj/p1449/p1449.cpp
+++ boj/p1449/p1449.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 int main() {
 	int n, l, a[1001];
+	for (int i = 1; i < 1001; i++)
+		a[i] = 0;
 	cin >> n >> l;
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-
+	for (int i = 0; i < n; i++) {
+		int h;
+		cin >> h;
+		a[h] = 1;
+	}
+	int cnt = 0;
+	for (int i = 1; i < 1001; i++) {
+		if (a[i]) {
+			i += l - 1;
+			cnt++;
+		}
+	}
+	cout << cnt;
 }
