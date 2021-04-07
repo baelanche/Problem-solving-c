c60426de0ce2a34189722454742fe79d4849d40f
baelanche <baelanche@naver.com>
2021-04-07 21:40:50 +0900

    20210407

--- boj/p11047/p11047.cpp
+++ boj/p11047/p11047.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n, k, a[10];
+	int n, k, a[10], ans = 0;
 	cin >> n >> k;
 	for (int i = 0; i < 10; i++)
 		cin >> a[i];
+	for (int i = n - 1; i >= 0; i--) {
+		ans += k / a[i];
+		k %= a[i];
+	}
+	cout << ans;
 }
